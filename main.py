263b783e2f5c3dcb1b9f4a20d13f1c7d9b27161c
Hrushal Nikhare <Hrushalnikhare@gmail.com>
2024-07-11 14:10:44 +0530

    Finished!!

--- main.py
+++ main.py
@@ -1,37 +1,62 @@
+#! /usr/bin/env python3
+"""
+Title: File Browser
+Author: Hrushal Nikhare
+
+"""
+
+# standard
 import os
-from rich import print
-import inquirer
-from rich.progress import track
-from rich.panel import Panel
-import json
 from os import walk
 import subprocess
 
-# print(os.path.abspath('config.json'))
+# rich
+from rich import print as rprint
+from rich.panel import Panel
+from rich.align import Align
 
-with open(os.path.abspath('config.json'),"r") as json_data:
-    data = json.load(json_data)
-print(data)
+# inquirer
+import inquirer
 
-def FileBrowser(last_dir=os.getcwd()):
+
+data = {
+    "FileOpener": "notepad",
+    "FileExplorer": "explorer",
+    "CodeEditor": "code",
+}
+code_file = ("py", "css", "json", "html", "js", "java", "cpp", "c", "php", "rb", "go")
+
+
+def file_browser(last_dir=os.getcwd()):
+    """
+    Function to browse files and directories
+    """
     files = []
     dirnames = []
 
-    for dirpath, dirnames, filenames in track(walk(last_dir)):
+    for _, dirnames, filenames in walk(last_dir):
+        # _ is dirpath
         files.extend(filenames)
         dirnames.extend(dirnames)
         break
 
     dirnames = list(set(dirnames))
-    # for dir in dirnames:
-    #     directory = ":open_file_folder: + dir"
 
     files = ["📄 " + file for file in files]
     dirnames = ["📂 " + dir for dir in dirnames]
 
-    print(Panel(f"\n📁 {last_dir}",title = "Current Folder",expand=False))
-    #list of choices
-    choices = files + dirnames + ["📁 Open in File Manager", "🔼 Go Up", "🏠 Go to root", "❌ Exit"]
+    rprint(
+        Align(
+            Panel(f"\n📁 {last_dir}", title="Current Folder", expand=False),
+            align="center",
+        )
+    )
+    # list of choices
+    choices = (
+        files
+        + dirnames
+        + ["📁 Open in File Manager", "🔼 Go Up", "🏠 Go to root", "❌ Exit"]
+    )
     questions = [
         inquirer.List(
             "Item",
@@ -41,40 +66,52 @@ def FileBrowser(last_dir=os.getcwd()):
     ]
 
     answers = inquirer.prompt(questions)
-    file = answers["Item"].split(" ", 1) + [last_dir]
-    return file
+    file_info = answers["Item"].split(" ", 1) + [last_dir]  # type: ignore
+    return file_info
 
 
-def FileHandler(selected_file, last_dir=os.getcwd()):
+def file_handler(selected_file, last_dir=os.getcwd()):
+    """
+    Function to handle the selected file
+    """
     if selected_file[0] == "📄":
-        # subprocess.Popen(["notepad", file[1]])
+        file_to_open = os.path.join(last_dir, selected_file[1])
         try:
-            subprocess.Popen([f"{data['FileOpener']}", os.path.join(last_dir, selected_file[1])])
-        except Exception as e:
-            subprocess.Popen(["notepad", os.path.join(last_dir, selected_file[1])])
+            if selected_file[1].split(".")[-1] in code_file:
+                subprocess.Popen([data["CodeEditor"], file_to_open], shell=True)
+            else:
+                subprocess.Popen([f"{data['FileOpener']}", file_to_open])
+
+        except (OSError, FileNotFoundError) as error:
+            rprint(f"Fatal Error: {error} \nOpening with default editor...")
+            subprocess.Popen(["notepad", file_to_open])
+
         finally:
             exit()
 
     elif selected_file[0] == "📂":
         new_dir = os.path.join(last_dir, selected_file[1])
         while True:
-            selected_file = FileBrowser(new_dir)
-            FileHandler(selected_file, new_dir)
+            selected_file = file_browser(new_dir)
+            file_handler(selected_file, new_dir)
 
     elif selected_file[0] == "🔼":
         new_dir = os.path.dirname(last_dir)
         while True:
-            selected_file = FileBrowser(new_dir)
-            FileHandler(selected_file, new_dir)
+            selected_file = file_browser(new_dir)
+            file_handler(selected_file, new_dir)
 
     elif selected_file[0] == "🏠":
         home_dir = os.path.abspath(".").split(os.path.sep)[0] + os.path.sep
         while True:
-            selected_file = FileBrowser(home_dir)
-            FileHandler(selected_file, home_dir)
+            selected_file = file_browser(home_dir)
+            file_handler(selected_file, home_dir)
 
     elif selected_file[0] == "📁":
-        subprocess.Popen(["explorer", last_dir])
+        try:
+            subprocess.Popen([data["FileExplorer"], last_dir])
+        except (OSError, FileNotFoundError):
+            subprocess.Popen(["explorer", last_dir])
         exit()
 
     elif selected_file[0] == "❌":
@@ -82,9 +119,6 @@ def FileHandler(selected_file, last_dir=os.getcwd()):
 
 
 while True:
-    file = FileBrowser()
-    FileHandler(file, file[2])
+    file = file_browser()
+    file_handler(file, file[2])
 
-# print("🫡 You selected: [bold red]" + answers["Item"] + "[/bold red]")
-# subprocess.Popen(["notepad",file[1]])
-# pprint(answers)
